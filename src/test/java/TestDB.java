61269e3b3e0a014eb3f5e9e8c31c2400353f0be1
Administrator <Administrator@WIN-3NHABA63FSJ>
2017-06-01 13:55:50 +0900

    06.01 13:55

---
+++ src/test/java/TestDB.java
@@ -0,0 +1,67 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestDB {
+	
+	@Autowired
+	DataSource ds;
+	
+	@Autowired
+	SqlSessionFactoryBean factory;
+	
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	
+//	private ApplicationContext ctx;
+//	
+//	@Before
+//	public void setup() {
+//		ctx = new ClassPathXmlApplicationContext(
+//				"classpath:applicationContext.xml");
+//	}
+	
+	@Test
+	public void testSqlSession() {
+//		SqlSessionTemplate session =
+//				ctx.getBean("sqlSession", SqlSessionTemplate.class);
+		Assert.assertNotNull(sqlSession);
+	}
+	
+	@Test
+	public void testSqlSessionFactory() {
+//		SqlSessionFactoryBean factory = 
+//				ctx.getBean("sqlSessionFactory", SqlSessionFactoryBean.class);
+		Assert.assertNotNull(factory);
+	}
+	
+	@Test
+	@Ignore
+	public void testDatasource() {
+		//DataSource ds = ctx.getBean("dataSource", DataSource.class);
+		try {
+			Connection conn = (Connection)ds.getConnection();
+			Assert.assertNotNull(conn);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+}
